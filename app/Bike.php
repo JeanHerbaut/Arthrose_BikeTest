57920aff5227bf021590a94a65cf2e55ef44f984
Robiiiiiiiiiiiin <robinbecherraz2@hotmail.fr>
2020-06-22 11:54:21 +0200

    Update Bike.php

--- app/Bike.php
+++ app/Bike.php
@@ -4,7 +4,19 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Bikes extends Model
+class Bike extends Model
 {
-    //
+    protected $fillable = ['product_id', 'size'];
+
+    public function product() {
+        return $this->hasOne(Product::class);
+    }
+
+    public function vtt() {
+        return $this->hasOne(Vtt::class);
+    }
+
+    public function ebike() {
+        return $this->hasOne(Ebike::class);
+    }
 }
