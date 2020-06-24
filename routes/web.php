be9e8eddbbddd3a064fd1854bc3f4c7e3d92acaa
Jean Herbaut <jean.herbaut@heig-vd.ch>
2020-06-24 09:55:38 +0200

    admin

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,5 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/billeterie', 'BilleterieController@rendPage');
+
+Route::get('/admin/consultation', 'AdminController@getData');
