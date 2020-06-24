a0832582005cbe3d04b4ff7423ea9fc11becd1bc
Robiiiiiiiiiiiin <robinbecherraz2@hotmail.fr>
2020-06-24 12:50:55 +0200

    update seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(PersonSeeder::class);
         $this->call(UserSeeder::class);
+        $this->call(EventSeeder::class);
+        $this->call(EditionSeeder::class);
+        $this->call(TestScheduleSeeder::class);
     }
 }
