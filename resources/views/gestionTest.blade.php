f1b72420b33edaaf2c24c3285883c39853a76b2a
Saiykoh <kevin.kopp20@gmail.com>
2020-06-25 16:32:27 +0200

    Ajout majeur gestionTest, ajout pop up pas fini

--- resources/views/gestionTest.blade.php
+++ resources/views/gestionTest.blade.php
@@ -1,182 +1,207 @@
 @extends('layouts.template')
 @section('content')
-<link defer rel="stylesheet" type="text/css" href="{{ asset('css/gestionTest.css') }}" >
+<link defer rel="stylesheet" type="text/css" href="{{ asset('css/gestionTest.css') }}">
 <div class="container">
     <section>
-      <div class="container-test">
-        <div class="header-test">
+        <!-- The Modal -->
+        <div id="myModal" class="modal">
 
-          <p>Disponibles</p>
-        </div>
-        <div class="vignette vignette-start">
-          <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
-          <a href="#">Commencer</a>
-        </div>
-        <div class="vignette vignette-start">
-          <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
-          <a href="#">Commencer</a>
-        </div>
-        <div class="vignette vignette-start">
-          <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
-          <a href="#">Commencer</a>
-        </div>
-        <div class="vignette vignette-start">
-          <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
-          <a href="#">Commencer</a>
-        </div>
-        <div class="vignette vignette-start">
-          <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
-          <a href="#">Commencer</a>
-        </div>
-        <div class="vignette vignette-start">
-          <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
-          <a href="#">Commencer</a>
-        </div>
-        <div class="vignette vignette-start">
-          <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
-          <a href="#">Commencer</a>
-        </div>
-        <div class="vignette vignette-start">
-          <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
-          <a href="#">Commencer</a>
-        </div>
-        <div class="vignette vignette-start">
-          <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
-          <a href="#">Commencer</a>
-        </div>
-        <div class="vignette vignette-start">
-          <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
-          <a href="#">Commencer</a>
-        </div>
-        <div class="vignette vignette-start">
-          <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
-          <a href="#">Commencer</a>
-        </div>
-        <div class="vignette vignette-start">
-          <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
-          <a href="#">Commencer</a>
-        </div>
-        <div class="vignette vignette-start">
-          <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
-          <a href="#">Commencer</a>
-        </div>
-        <div class="vignette vignette-start">
-          <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
-          <a href="#">Commencer</a>
-        </div>
-        <div class="vignette vignette-start">
-          <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
-          <a href="#">Commencer</a>
-        </div>
-        <div class="vignette vignette-start">
-          <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
-          <a href="#">Commencer</a>
-        </div>
-        <div class="vignette vignette-start">
-          <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
-          <a href="#">Commencer</a>
-        </div>
-        <div class="vignette vignette-start">
-          <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
-          <a href="#">Commencer</a>
-        </div>
-        <div class="vignette vignette-start">
-          <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
-          <a href="#">Commencer</a>
-        </div>
-        <div class="vignette vignette-start">
-          <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
-          <a href="#">Commencer</a>
-        </div>
-        <div class="vignette vignette-start">
-          <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
-          <a href="#">Commencer</a>
-        </div>
-        <div class="vignette vignette-start">
-          <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
-          <a href="#">Commencer</a>
-        </div>
-        <div class="vignette vignette-start">
-          <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
-          <a href="#">Commencer</a>
-        </div>
-        
+            <!-- Modal content -->
+            <div class="modal-content">
+                <div class="wrapper-left">
+                    <span class="close">&times;</span>
+                    <p>Création test</p>
+                    <div class="vignette-test">
+                        <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
+                        <p><strong>Titanium 370-X</strong></p>
+                        <p>SCOTT</p>
+                    </div>
+                    <p>Le test commence à: </br>
+                        <span>14:36</span></p>
+                    <p>Le test commence à: </br>
+                        <span>15:06</span></p>
+                </div>
+                <div class="wrapper-right">
+                    <div class="">
+                        <p></p>
+                    </div>
+                </div>
+            </div>
 
-      </div>
-      <div class="container-test">
-        <div class="header-test">
-          <p>En cours..</p>
         </div>
-        <div class="vignette vignette-end">
-          <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
-          <a href="#">Terminer</a>
+        <div class="container-test">
+            <div class="header-test">
+                <p>Disponibles</p>
+            </div>
+            <div class="vignette vignette-start">
+                <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
+                <a href="#" class="myBtn">Commencer</a>
+            </div>
+            <div class="vignette vignette-start">
+                <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
+                <a href="#">Commencer</a>
+            </div>
+            <div class="vignette vignette-start">
+                <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
+                <a href="#">Commencer</a>
+            </div>
+            <div class="vignette vignette-start">
+                <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
+                <a href="#">Commencer</a>
+            </div>
+            <div class="vignette vignette-start">
+                <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
+                <a href="#">Commencer</a>
+            </div>
+            <div class="vignette vignette-start">
+                <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
+                <a href="#">Commencer</a>
+            </div>
+            <div class="vignette vignette-start">
+                <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
+                <a href="#">Commencer</a>
+            </div>
+            <div class="vignette vignette-start">
+                <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
+                <a href="#">Commencer</a>
+            </div>
+            <div class="vignette vignette-start">
+                <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
+                <a href="#">Commencer</a>
+            </div>
+            <div class="vignette vignette-start">
+                <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
+                <a href="#">Commencer</a>
+            </div>
+            <div class="vignette vignette-start">
+                <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
+                <a href="#">Commencer</a>
+            </div>
+            <div class="vignette vignette-start">
+                <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
+                <a href="#">Commencer</a>
+            </div>
+            <div class="vignette vignette-start">
+                <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
+                <a href="#">Commencer</a>
+            </div>
+            <div class="vignette vignette-start">
+                <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
+                <a href="#">Commencer</a>
+            </div>
+            <div class="vignette vignette-start">
+                <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
+                <a href="#">Commencer</a>
+            </div>
+            <div class="vignette vignette-start">
+                <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
+                <a href="#">Commencer</a>
+            </div>
+            <div class="vignette vignette-start">
+                <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
+                <a href="#">Commencer</a>
+            </div>
+            <div class="vignette vignette-start">
+                <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
+                <a href="#">Commencer</a>
+            </div>
+            <div class="vignette vignette-start">
+                <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
+                <a href="#">Commencer</a>
+            </div>
+            <div class="vignette vignette-start">
+                <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
+                <a href="#">Commencer</a>
+            </div>
+            <div class="vignette vignette-start">
+                <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
+                <a href="#">Commencer</a>
+            </div>
+            <div class="vignette vignette-start">
+                <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
+                <a href="#">Commencer</a>
+            </div>
+            <div class="vignette vignette-start">
+                <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
+                <a href="#">Commencer</a>
+            </div>
+
+
         </div>
-        <div class="vignette vignette-end">
-          <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
-          <a href="#">Terminer</a>
+        <div class="container-test">
+            <div class="header-test">
+                <p>En cours..</p>
+            </div>
+            <div class="vignette vignette-end">
+                <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
+                <a href="#" class="myBtn">Terminer</a>
+            </div>
+            <div class="vignette vignette-end">
+                <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
+                <a href="#">Terminer</a>
+            </div>
+            <div class="vignette vignette-end">
+                <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
+                <a href="#">Terminer</a>
+            </div>
+            <div class="vignette vignette-end">
+                <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
+                <a href="#">Terminer</a>
+            </div>
+            <div class="vignette vignette-end">
+                <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
+                <a href="#">Terminer</a>
+            </div>
+            <div class="vignette vignette-end">
+                <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
+                <a href="#">Terminer</a>
+            </div>
+            <div class="vignette vignette-end">
+                <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
+                <a href="#">Terminer</a>
+            </div>
+            <div class="vignette vignette-end">
+                <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
+                <a href="#">Terminer</a>
+            </div>
+            <div class="vignette vignette-end">
+                <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
+                <a href="#">Terminer</a>
+            </div>
+            <div class="vignette vignette-end">
+                <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
+                <a href="#">Terminer</a>
+            </div>
+            <div class="vignette vignette-end">
+                <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
+                <a href="#">Terminer</a>
+            </div>
+            <div class="vignette vignette-end">
+                <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
+                <a href="#">Terminer</a>
+            </div>
+            <div class="vignette vignette-end">
+                <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
+                <a href="#">Terminer</a>
+            </div>
+            <div class="vignette vignette-end">
+                <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
+                <a href="#">Terminer</a>
+            </div>
+            <div class="vignette vignette-end">
+                <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
+                <a href="#">Terminer</a>
+            </div>
+            <div class="vignette vignette-end">
+                <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
+                <a href="#">Terminer</a>
+            </div>
+            <div class="vignette vignette-end">
+                <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
+                <a href="#">Terminer</a>
+            </div>
         </div>
-        <div class="vignette vignette-end">
-          <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
-          <a href="#">Terminer</a>
-        </div>
-        <div class="vignette vignette-end">
-          <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
-          <a href="#">Terminer</a>
-        </div>
-        <div class="vignette vignette-end">
-          <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
-          <a href="#">Terminer</a>
-        </div>
-        <div class="vignette vignette-end">
-          <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
-          <a href="#">Terminer</a>
-        </div>
-        <div class="vignette vignette-end">
-          <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
-          <a href="#">Terminer</a>
-        </div>
-        <div class="vignette vignette-end">
-          <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
-          <a href="#">Terminer</a>
-        </div>
-        <div class="vignette vignette-end">
-          <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
-          <a href="#">Terminer</a>
-        </div>
-        <div class="vignette vignette-end">
-          <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
-          <a href="#">Terminer</a>
-        </div>
-        <div class="vignette vignette-end">
-          <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
-          <a href="#">Terminer</a>
-        </div>
-        <div class="vignette vignette-end">
-          <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
-          <a href="#">Terminer</a>
-        </div>
-        <div class="vignette vignette-end">
-          <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
-          <a href="#">Terminer</a>
-        </div>
-        <div class="vignette vignette-end">
-          <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
-          <a href="#">Terminer</a>
-        </div>
-        <div class="vignette vignette-end">
-          <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
-          <a href="#">Terminer</a>
-        </div>
-        <div class="vignette vignette-end">
-          <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
-          <a href="#">Terminer</a>
-        </div>
-        <div class="vignette vignette-end">
-          <img class="velo-img" src="{{ asset('img/bike.png') }}" alt="">
-          <a href="#">Terminer</a>
-        </div>
-      </div>
     </section>
 </div>
-
+<script src="{{ asset('js/gestionTest.js')}}" type="text/javascript" defer></script>
 @endsection
