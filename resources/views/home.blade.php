5b8f4597d94a9220a6662123d254c7f2c4ec75c7
Saiykoh <kevin.kopp20@gmail.com>
2020-07-08 14:29:14 +0200

    update logo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,14 @@
 @extends('layouts.template')
-
+  <link defer rel="stylesheet" type="text/css" href="{{ asset('css/home.css') }}">
 @section('content')
-<video class="video" width="100%" height="auto" autoplay muted>
+
+
+
+<video id="myVideo" loop class="video" width="100%" height="auto" autoplay muted>
     <source src="{{ asset('img/1748463785.mp4') }}" type="video/mp4">
 </video>
-<h1>HOME</h1>
-<p> A la recherche d'un VTT, d'un e-bike ou juste d'un "vélo"? Le top c'est encore de l'essayer avant de l'acheter!!!
-    Le meilleur VTT ou e-bike est celui qui te convient le mieux!
-    Rien ne vaut un test dans le terrain pour trouver le bon modèle. Plus de 500 bikes en test t’attendent pour un ride dans le terrain.  Les représentants des différentes marques t’aideront à trouver le bike idéal pour toi.
-    Viens comparer les VTT, les e-bikes, les gravels des différents segments, trouve la bonne géométrie, découvrir le bike que tu ne connaissais pas. Ici tu peux juger et comparer les divers modèles sur les parcours spécialement balisés pour cet évènement.
-    Fait ton choix et c’est parti pour un essai...
-    Pour 2020 une liste d'hotels et d'hebergements sera disponible. Les participants profiteront de tarifs préferenciels dans ces etablissements. Liste disponible dès juillet 2020...
-    Et n'oublie pas ton casque et une pièce d'identité...
-</p>
+<video id="myVideoMobile" loop class="video" width="100%" height="auto" autoplay muted>
+    <source src="{{ asset('img/video-crop.mp4') }}" type="video/mp4">
+</video>
+
 @endsection
