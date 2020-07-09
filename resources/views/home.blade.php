a28ca73717442d4cb245f40ecde590c8fcdd3a1f
Saiykoh <kevin.kopp20@gmail.com>
2020-07-09 09:26:19 +0200

    upadte

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,11 +4,13 @@
 
 
 
-<video id="myVideo" loop class="video" width="100%" height="auto" autoplay muted>
+<video id="myVideo" loop class="video"  height="auto" autoplay muted>
     <source src="{{ asset('img/1748463785.mp4') }}" type="video/mp4">
 </video>
-<video id="myVideoMobile" loop class="video" width="100%" height="auto" autoplay muted>
+<video id="myVideoMobile" loop class="video"  height="auto" autoplay muted>
     <source src="{{ asset('img/video-crop.mp4') }}" type="video/mp4">
 </video>
+
+<img id="imgHome" src="{{ asset('img/logoIcone_BTG.png') }}" alt="">
 
 @endsection
