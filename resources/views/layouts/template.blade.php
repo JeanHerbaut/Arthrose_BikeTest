c814808e147fa41c759cc68fb38fc178a663fd5f
Saiykoh <kevin.kopp20@gmail.com>
2020-07-10 08:09:39 +0200

    up

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -9,6 +9,7 @@
   <link rel="stylesheet" type="text/css" href="{{ asset('css/admin.css') }}">
   <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
   <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
+  <link rel="icon" href="{{ asset('img/icon.ico') }}" />
 </head>
 
 <body>
